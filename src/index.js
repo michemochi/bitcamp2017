206840b7d9b3576e5c4b7ffeb98f8f6655787a03
chrismamo1 <christophermcalpine@gmail.com>
2017-04-08 00:41:33 -0600

    will thiskill me?

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+/*ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);*/
+
+var api_key = 'TFGsLFJYAH8AQ1iX3LFejydqvYlqVP2fsLqyBjgj';
+
+function doSearch(q, offset=0) {
+          var result =
+                      fetch('https://api.nal.usda.gov/ndb/search?api_key=' + api_key + '&q=' + q + '&format=json&offset=' + offset);
+            result = JSON.parse(result);
+              var retval = result.list.item;
+                if (result.list.total > result.list.stop + 1) {
+                              return retval.concat(doSearch(q, result.list.end - result.list.start));
+                                } else {
+                                              retval;
+                                                }
+}
+
+ReactDOM.render(
+                  doSearch('Nutella'),
+                    document.getElementById('root')
+               );
